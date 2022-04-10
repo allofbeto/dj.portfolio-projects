b297b47d039ecd6e62d85c69ce46533c2eaf1309
Alberto Carrazco <carrazco2013@gmail.com>
2022-04-10 14:32:26 -0700

    we added AWS keys and signatures

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -106,6 +106,18 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = 'acportfoliostaticbucket'
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_ADDRESSING_STYLE = "virtual"
+
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
